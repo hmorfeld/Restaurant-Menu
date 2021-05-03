276fd008f7e9470d48c2ccb8754b78dfa5526ba1
hmorfeld <hmorfeld91@gmail.com>
2021-05-03 18:29:36 -0500

    Initial commit

---
+++ src/restaurant/Menu.java
@@ -0,0 +1,11 @@
+package restaurant;
+
+import java.util.ArrayList;
+
+public class Menu {
+    private int date;
+    private ArrayList <MenuItems>;
+
+    
+
+}
---
+++ src/restaurant/MenuItems.java
@@ -0,0 +1,12 @@
+package restaurant;
+
+import java.util.ArrayList;
+
+public class MenuItems {
+    private double price;
+    private String description;
+    private String category;
+    private String name;
+    private boolean isNew;
+
+}
